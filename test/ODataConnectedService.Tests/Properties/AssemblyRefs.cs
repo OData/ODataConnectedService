802eeee5f7c442132b7731030c7b774d9f0aa09f
Paul Odero <paodero@microsoft.com>
2020-03-03 16:06:31 +0300

    Adding test cases from Code Gen

---
+++ test/ODataConnectedService.Tests/Properties/AssemblyRefs.cs
@@ -0,0 +1,19 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="AssemblyRefs.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System;
+using System.Runtime.InteropServices;
+
+/// <summary>Current assembly reference</summary>
+internal static class AssemblyRef
+{
+        internal const string System = "System.dll";
+        internal const string SystemCore = "System.Core.dll";
+        internal const string SystemIO = "System.IO.dll";
+        internal const string SystemRuntime = "System.Runtime.dll";
+        internal const string SystemXml = "System.Xml.dll";
+        internal const string SystemXmlReaderWriter = "System.Xml.ReaderWriter.dll";
+}
