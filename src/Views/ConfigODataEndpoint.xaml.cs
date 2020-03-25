6899731fe0d1a8b2e5b87009ddbeffe6d4e002f0
Chebotov Nikolay <spiritkola@hotmail.com>
2020-03-25 22:53:17 +0300

    Fix #67 : Add ability for choosing local metadata files

--- src/Views/ConfigODataEndpoint.xaml.cs
+++ src/Views/ConfigODataEndpoint.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Microsoft.OData.ConnectedService.Views
@@ -13,6 +14,24 @@ namespace Microsoft.OData.ConnectedService.Views
         public ConfigODataEndpoint()
         {
             InitializeComponent();
+        }
+
+        private void OpenMetadataFileButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Win32.OpenFileDialog
+            {
+                DefaultExt = ".xml",
+                Filter = "OData Metadata Files (.xml)|*.xml",
+                Title = "Open OData Metadata File",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Endpoint.Text = openFileDialog.FileName;
+            }
         }
     }
 }
