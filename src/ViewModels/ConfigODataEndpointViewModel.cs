8ffa715557807e5cc8750fc6eda6aef3148bdaab
David K <dkaguma3@gatech.edu>
2020-03-25 20:46:43 +0300

    namespace cleanup

--- src/ViewModels/ConfigODataEndpointViewModel.cs
+++ src/ViewModels/ConfigODataEndpointViewModel.cs
@@ -2,11 +2,9 @@
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Net;
-using System.Security;
 using System.Threading.Tasks;
 using System.Xml;
 using Microsoft.OData.ConnectedService.Common;
