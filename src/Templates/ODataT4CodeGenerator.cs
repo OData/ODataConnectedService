4817b8182668f8541d7e4d739d4410c938ed3b90
Chebotov Nikolay <spiritkola@hotmail.com>
2020-05-28 17:20:58 +0300

    Fix #133

--- src/Templates/ODataT4CodeGenerator.cs
+++ src/Templates/ODataT4CodeGenerator.cs
@@ -43,15 +43,12 @@ namespace Microsoft.OData.ConnectedService.Templates
         {
 
 /*
-OData Client T4 Template ver. 2.4.0
-Copyright (c) Microsoft Corporation
-All rights reserved.
-MIT License
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------
+// <copyright file="ODataT4CodeGenerator.ttinclude" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors.  All rights reserved.
+//      See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------------------
 */
 
 /*
@@ -3607,6 +3604,12 @@ internal static class Utils
                     if (valueClrType.Equals(clientTemplate.StringTypeName))
                     {
                         defaultValue = "\"" + defaultValue + "\"";
+                    }
+                    else if (valueClrType.Equals(clientTemplate.BooleanTypeName))
+                    {
+                        // EDMX specifies boolean defaults with capital letter, C# needs this string to be lower case.
+                        if (isCSharpTemplate)
+                            defaultValue = defaultValue.ToLower();
                     }
                     else if (valueClrType.Equals(clientTemplate.BinaryTypeName))
                     {
