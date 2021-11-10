8fc82ddd5f6a4d719a9b1b836438fac7d7b29d1a
ElizabethOkerio <elizaokerio@gmail.com>
2021-11-10 08:27:52 +0300

    add a console app

---
+++ src/ODataConnectedServiceCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ODataConnectedServiceCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
