3fb3655496d2f9dc76674664ad3deb33e1290e88
Elizabeth Okerio <elokerio@microsoft.com>
2021-11-25 12:29:09 +0300

    add a console app (#235)
    * restructure project and add a console app
    
    * update project names
    
    * update namespace

---
+++ src/OData.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace OData.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+
+            app.Name = "odata-cli";
+            app.Description = "Client Proxy generator for OData endpoints.";
+
+            // Set the arguments to display the description and help text
+            app.HelpOption("-?|-h|--help");
+        }
+    }
+}
