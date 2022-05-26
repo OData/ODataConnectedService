662843ffe12a8f7e2ba205723a43ad013f7e8be6
Jesper den Boer <jesper.denboer@inbisco.nl>
2022-05-26 22:50:37 +0200

    Fix build.cmd by updating visualstudioversion to 17 and add vs 2019 and 2022 locations
    -Building project with vs 2017 and 2019 does not work.
    -Migrated deprecated Microsoft.CodeAnalysis.FxCopAnalyzers package to Microsoft.CodeAnalysis.NetAnalyzers.

--- build.cmd
+++ build.cmd
@@ -7,12 +7,18 @@ mkdir bin
 
 :Build
 
-REM Find one of the Visual Studio 2017 installations.
+REM Find one of the Visual Studio 2017/2019/2022 installations.
 REM Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the
 REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
 set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
 if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" 
+if not exist %MSBuild% @set MSBuild="%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles%\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 if "%1" == "" goto BuildDefaults
 
