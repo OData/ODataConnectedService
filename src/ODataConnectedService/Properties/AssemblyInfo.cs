5aa14a29333222954f7a7dea3bcee9ff6869e5ed
Elizabeth Okerio <elizaokerio@gmail.com>
2022-07-13 16:54:42 +0300

    support vs2022 (#295)
    * support vs2022
    
    * fix build error
    
    * update based on review comments
    
    * update based on review comments
    
    * uodate based on review comments

--- src/ODataConnectedService/Properties/AssemblyInfo.cs
+++ src/ODataConnectedService/Properties/AssemblyInfo.cs
@@ -38,8 +38,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.13.0.0")]
-[assembly: AssemblyFileVersion("0.13.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: InternalsVisibleTo("ODataConnectedService.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003d2fbcc3c5595b4212227a5ca0fb1c45cb9c942096831420563dc169dd847a8a5fe068170bd10802dea50bc4d8342c1f8aa2ab9f27e3ceccdb21ce8c1415c3ea33eb7bc09059ca50b3e8607fa34bb9925466525ceffc3edac84ac1863afc28ef935c8db7a6430771fa41aa3de8a8d399990b02cae85e834e5d29d2cc0109b8cb")]
