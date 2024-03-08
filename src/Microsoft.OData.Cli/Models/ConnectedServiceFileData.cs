380dae5177358c0dba76c87888019c6657a9602c
Greg Winterstein <github0202@kerplonk.com>
2024-03-08 08:11:53 -0800

    Comment public properies as requested in pull request comments

--- src/Microsoft.OData.Cli/Models/ConnectedServiceFileData.cs
+++ src/Microsoft.OData.Cli/Models/ConnectedServiceFileData.cs
@@ -14,8 +14,14 @@ namespace Microsoft.OData.Cli.Models
     /// </summary>
     public class ConnectedServiceFileData
     {
+        /// <summary>
+        /// Gets or sets the unique Provider ID for the provider/handler. This is used to match a provider with its handlers.
+        /// </summary>
         public string ProviderId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the OData Connected Service user settings extracted from ConnectedService.json.
+        /// </summary>
         public BaseUserSettings ExtendedData { get; set; }
     }
 }
